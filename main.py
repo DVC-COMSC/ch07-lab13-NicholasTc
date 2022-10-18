541a0abfca0f039c810582fa6cf41e79e81e2548
Nicholas <87298104+NicholasTc@users.noreply.github.com>
2022-10-18 12:03:14 -0700

    Update main.py

--- main.py
+++ main.py
@@ -15,3 +15,18 @@ cnum = len(numbers[0])
 # ******************************
 # Make your Code
 # ******************************
+
+cross_count = 0
+
+for i in range(rnum):
+    for j in range(cnum):
+        if numbers[i][j] == 1:
+            # check for it's neighbors
+            try:
+                if numbers[i + 1][j-1] == 1 and numbers[i + 1][j] == 1 and numbers[i + 1][j + 1] == 1 and numbers[i + 2][j] == 1:
+                    cross_count += 1
+            except IndexError:
+                continue
+
+print(cross_count)
+        
